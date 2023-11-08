cabba32c04ae5d3447e74c2f8f81d60240e698c1
Facundo Maciel <facundomaciel12@hotmail.com>
2023-11-08 17:28:53 -0300

    sorting elements

--- src/components/home/Home.tsx
+++ src/components/home/Home.tsx
@@ -1,17 +1,30 @@
 import myImage from "../../assets/images/fotoPortada.png"
 import button from "../../assets/icons/centerButton.png"
+import gitHub from "../../assets/icons/iconGitHub.png"
+import linkedIn from "../../assets/icons/iconLinkedin.png"
+import whatsApp from "../../assets/icons/iconWhatsapp.png"
+import sentEmail from "../../assets/icons/SentEmail.png"
+import downloadCV from "../../assets/icons/cvDownload.png"
+
 
 const Home = () => {
   return (
     <section className="flex">
 
-      <aside className="w-[65%] text-center">
-        <h1 className="text-6xl font-bold">
+      <aside className="w-[65%] items-end">
+        <div className="absolute left-0 bottom-0">
+        <h1 className="text-6xl text-[#232323] font-bold ml-6 mb-10">
           Facundo
           <p className="">
             Maciel
           </p>
         </h1>
+        <figure className="flex ml-6">
+          <img src={gitHub} alt="Linked icon to GitHub" />
+          <img src={linkedIn} alt="Linked icon to LinkedIn" />
+          <img src={whatsApp} alt="Linked icon to WhatsApp" />
+        </figure>
+        </div>
       </aside>
 
       <article>
@@ -22,12 +35,17 @@ const Home = () => {
           <img src={myImage} alt="My Image" className="w-full" />
         </figure>
       </article>
+
       <div className="flex flex-col w-[8%]">
         <aside className="bg-[#232323] h-full">
-          
+          <figure className="flex justify-end">
+            <img src={sentEmail} alt="Icon to sent an email" className="p-2"/>
+          </figure>
         </aside>
         <aside className="bg-[#FF1D1D] h-full">
-          
+          <figure className="flex justify-end">
+            <img src={downloadCV} alt="Download cv icon" className="p-2"/>
+          </figure>
         </aside>
       </div>
 
